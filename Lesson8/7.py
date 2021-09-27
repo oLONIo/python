1d23b026727c9270a016a26366eafe333671a3b4
oLONIo <zomby2005@gmail.com>
2021-09-28 00:40:09 +0300

    .

---
+++ Lesson8/7.py
@@ -0,0 +1,24 @@
+class ComplexNumber:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.z = 'a + b * i'
+
+    def __add__(self, other):
+        print(f'Сумма t1 и t2 равна')
+        return f'z = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение t1 и t2 равно')
+        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'z = {self.a} + {self.b} * i'
+
+
+
+t1 = ComplexNumber(31, -32)
+t2 = ComplexNumber(10, 50)
+print(t1)
+print(t1 + t2)
+print(t1 * t2)
