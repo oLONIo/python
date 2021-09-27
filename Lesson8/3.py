57d0d07231b9962fd7ca4b97a5e8c02778699360
oLONIo <zomby2005@gmail.com>
2021-09-27 23:28:25 +0300

    .

---
+++ Lesson8/3.py
@@ -0,0 +1,28 @@
+class OnlyInt:
+
+    def __init__(self):
+        self.my_list = []
+
+    def inpt(self):
+
+        q = int(input('Введите значение: '))
+        self.my_list.append(q)
+
+        while True:
+            try:
+                q = int(input('Введите значение: '))
+                self.my_list.append(q)
+            except:
+                print(f'Не цифра!')
+                print(f'\nТекущий список: {self.my_list}\n')
+                yn = input(f'Ввести ещё раз? Y/N ')
+
+                if (yn) == 'Y' or yn == 'y':
+                    print(test.inpt())
+                elif yn == 'N' or yn == 'n':
+                    print('Выход')
+                    exit()
+
+test = OnlyInt()
+print(test.inpt())
+
